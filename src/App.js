242e279438c720d0b03ee3e8f4cb31074330de7a
Richard Narh <rkklinks@gmail.com>
2023-07-14 05:24:37 +0000

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
+import card_picture from './avat.jpeg';
+import xbox from './xbox.svg'
 import './App.css';
+
+
+function Card () {
+  return (
+   <>
+    <article className='App-card'>
+      <img src={card_picture} className='App-card-image' />
+      <p className='App-card-text'><em>Avatar : frontier of pandora</em></p>
+      <div className='App-card-platform'>
+      
+      <img src={xbox} className='platform-sf'/>
+
+      </div>
+    
+    </article>
+
+   </>
+
+  );
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Kwakwe last stop
-
-        </a>
+      <Card />
       </header>
+
+     
     </div>
   );
 }
